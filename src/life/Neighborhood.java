8f2fc030c123061df5a8b8cf80b3e3d3273a2c1f
Lawson-C <lawsoncrutcher@gmail.com>
2021-11-07 23:16:55 -0500

    created graphic representation of game of life

--- src/life/Neighborhood.java
+++ src/life/Neighborhood.java
@@ -1,16 +1,34 @@
 package life;
 
+import life.patterns.Pattern;
+import processing.core.PApplet;
+
 public class Neighborhood {
+    PApplet window;
     Cell[][][] cells;
 
-    public Neighborhood() {
+    public Neighborhood(PApplet window, Pattern p, int lx, int ly, int lz) {
+        this.window = window;
+        this.cells = new Cell[lx][ly][lz];
+        for (int x = 0; x < lx; x++) {
+            for (int y = 0; y < ly; y++) {
+                for (int z = 0; z < lz; z++) {
+                    float state = p.getState(x, y, z, 0, 1);
+                    this.cells[x][y][z] = new Cell(window, x, y, z, state);
+                }
+            }
+        }
     }
 
     public void display() {
         for (Cell[][] c1 : cells) {
             for (Cell[] c2 : c1) {
                 for (Cell c : c2) {
+                    this.window.pushMatrix();
+                    this.window.translate(-this.cells.length / 2 * Cell.SIZE, -this.cells[0].length / 2 * Cell.SIZE,
+                            -this.cells[0][0].length / 2 * Cell.SIZE);
                     c.display();
+                    this.window.popMatrix();
                 }
             }
         }
--- src/windows/Life.java
+++ src/windows/Life.java
@@ -1,21 +1,29 @@
 package windows;
 
 import life.Neighborhood;
+import life.patterns.Random;
 import processing.core.PApplet;
 
-public class Life extends PApplet{
+public class Life extends PApplet {
     private Neighborhood neighborhood;
 
     public Life() {
-        neighborhood = new Neighborhood();
+        neighborhood = new Neighborhood(this, new Random(), 16, 16, 16);
         super.runSketch();
     }
 
     public void settings() {
-        size(800, 800);
+        size(800, 800, P3D);
     }
 
     public void draw() {
-        ellipse(mouseX, mouseY, 25, 25);
+        background(0, 0, 255);
+
+        translate(width / 2, height / 2);
+        rotateY(map(mouseX, 0, width, -TAU, TAU));
+        rotateX(-map(mouseY, 0, height, -TAU, TAU));
+        translate(-width / 2, -height / 2);
+
+        neighborhood.display();
     }
 }
